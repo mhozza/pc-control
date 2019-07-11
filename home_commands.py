bb7b19745bb9c0a7a0585908efdc03c43d779d6b
mhozza <mhozza@gmail.com>
2019-07-11 22:40:23 +0100

    Wrap server in the server class and don't laod the config on module import.

--- home_commands.py
+++ home_commands.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import platform
 import toml
@@ -7,13 +8,16 @@ from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 
-config = toml.load(Path(__file__).parent / "config.toml")
 
-KEY = config["key"]
 SLEEP_COMMAND = "SUSPEND"
 
 
+def load_config():
+    return toml.load(Path(__file__).parent / "config.toml")
+
+
 def sleep():
+    logging.debug("#sleep")
     if platform.system() == "Windows":
         os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
     else:
@@ -21,26 +25,45 @@ def sleep():
         os.system("systemctl suspend")
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        url = urlparse(self.path)
-        params = parse_qs(url.query)
-        print(params)
-        if params.get("key", [None])[0] == KEY:
-            if params.get("command", [None])[0] == SLEEP_COMMAND:
-                sleep()
-        return
+def request_handler_factory(config):
+    class RequestHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            try:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                url = urlparse(self.path)
+                params = parse_qs(url.query)
+                logging.debug(params)
+                if params.get("key", [None])[0] == config["key"]:
+                    if params.get("command", [None])[0] == SLEEP_COMMAND:
+                        sleep()
+            except Exception as e:
+                logging.exception(e)
+
+    return RequestHandler
 
 
-def run():
-    server_address = ("0.0.0.0", config.get("port", 8001))
-    print("starting server...", server_address)
-    httpd = HTTPServer(server_address, RequestHandler)
-    print("running server...")
-    httpd.serve_forever()
+class Server:
+    def __init__(self):
+        self.config = load_config()
+        logging.debug(self.config)
+        server_address = ("0.0.0.0", self.config.get("port", 8001))
+        logging.info("setting up server... %s", server_address)
+        self.server = HTTPServer(server_address, request_handler_factory(self.config))
+        logging.debug(self.server)
+
+    def start(self):
+        logging.info("running server...")
+        self.server.serve_forever()
+        logging.info("finished.")
+
+    def stop(self):
+        self.server.shutdown()
+        logging.debug("shuting down server")
 
 
-run()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    server = Server()
+    server.start()
